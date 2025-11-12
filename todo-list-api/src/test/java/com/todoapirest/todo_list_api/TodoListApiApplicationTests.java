cda6f82946300190dbb76fb88992a33e8a06b485
Luiz Messias <luizgdsmdev@gmail.com>
2025-11-12 18:48:00 -0300

    automated testing (Todo-list-API): adding basic structure for route testing.

--- todo-list-api/src/test/java/com/todoapirest/todo_list_api/TodoListApiApplicationTests.java
+++ todo-list-api/src/test/java/com/todoapirest/todo_list_api/TodoListApiApplicationTests.java
@@ -1,13 +1,34 @@
 package com.todoapirest.todo_list_api;
 
+import com.todoapirest.todo_list_api.Entity.Todo;
+import com.todoapirest.todo_list_api.TodoDataTransferObject.TodoCreateRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TodoListApiApplicationTests {
+    @Autowired
+    private WebTestClient webTestClient;
 
+    //Todo creation tests
 	@Test
-	void contextLoads() {
+	void creationSuccess() {
+        TodoCreateRequest requestBody = new TodoCreateRequest("Title", false, "description", 3);
+
+        webTestClient
+        .post()
+        .uri("/todo")
+        .bodyValue(requestBody)
+        .exchange()
+        .expectStatus().isCreated()
+        .expectBody()
+        .jsonPath("$.title").isEqualTo("Title")
+        .jsonPath("$.description").isEqualTo("description")
+        .jsonPath("$.completed").isEqualTo(false)
+        .jsonPath("$.priority").isEqualTo(3)
+        .jsonPath("$.id").isNotEmpty();
 	}
 
 }
