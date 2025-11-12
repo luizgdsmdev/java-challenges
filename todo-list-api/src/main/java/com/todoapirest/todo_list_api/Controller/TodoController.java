0e08f3d5334260f4ea32469f92d2f1b9da09b546
Luiz Messias <luizgdsmdev@gmail.com>
2025-11-12 09:52:04 -0300

    Update route (Todo-list-API): adding better handling with response/exception for all routes on toDo dealing.

--- todo-list-api/src/main/java/com/todoapirest/todo_list_api/Controller/TodoController.java
+++ todo-list-api/src/main/java/com/todoapirest/todo_list_api/Controller/TodoController.java
@@ -1,5 +1,4 @@
 package com.todoapirest.todo_list_api.Controller;
-
 import com.todoapirest.todo_list_api.Entity.Todo;
 import com.todoapirest.todo_list_api.Exceptions.Records.ErrorResponse;
 import com.todoapirest.todo_list_api.Service.TodoService;
@@ -7,10 +6,8 @@ import com.todoapirest.todo_list_api.TodoDataTransferObject.TodoCreateRequest;
 import com.todoapirest.todo_list_api.TodoDataTransferObject.TodoUpdateRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,10 +20,20 @@ public class TodoController {
     public TodoController(TodoService todoService) {this.todoService = todoService;}
 
     @PostMapping
-    public ResponseEntity<Todo> create(@RequestBody @Valid TodoCreateRequest todoRequest){
-        Todo todoBody = todoService.createTodo(todoRequest);
-        URI location = URI.create("/todo/" + todoBody.getId());
-        return ResponseEntity.created(location).body(todoBody);
+    public ResponseEntity<Object> create(@RequestBody @Valid TodoCreateRequest todoRequest){
+        return todoService.createTodo(todoRequest)
+        .map(newTodo -> {
+            URI location = URI.create("/todo/" + newTodo.getId());
+            return ResponseEntity.created(location).body((Object) newTodo);
+        })
+        .orElseGet(() -> ResponseEntity
+        .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .body(new ErrorResponse(
+                "Bad Request",
+                "Malformed JSON in request body or invalid/non-existent id",
+                LocalDateTime.now()
+            ))
+        );
     }
 
     @PutMapping
@@ -34,27 +41,54 @@ public class TodoController {
         return todoService.updateTodo(todo)
         .map(updated -> ResponseEntity.status(HttpStatus.OK).body((Object) updated))
         .orElseGet(() -> ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(new ErrorResponse(
+                "Bad Request",
+                "Malformed JSON in request body or invalid/non-existent id",
+                LocalDateTime.now()
+            ))
+        );
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Object> getAll() {
+        return todoService.listAllTodos()
+        .map(todoList -> ResponseEntity.status(HttpStatus.OK).body((Object) todoList))
+        .orElseGet(() -> ResponseEntity
+        .status(HttpStatus.UNPROCESSABLE_ENTITY)
+        .body(new ErrorResponse(
+                "Unprocessable Entity",
+                "Malformed JSON in request body or invalid/non-existent id",
+                LocalDateTime.now()
+            ))
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getTodo(@PathVariable Long id){
+        return todoService.listTodo(id)
+        .map(todo -> ResponseEntity.ok().body( (Object) todo))
+        .orElseGet(() -> ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(
-                        "Todo not found",
-                        "ID " + todo.id() + " not found or don't exist, check for a valid payload",
+                        "Bad Request",
+                        "Invalid or non-existent id",
                         LocalDateTime.now()
                 ))
         );
     }
 
-    @GetMapping("/all")
-    public List<Todo> getAll() {
-        return todoService.listAllTodos();
-    }
-
-    @GetMapping("/{id}")
-    public Optional<Todo> getTodo(@PathVariable Long id){
-        return todoService.listTodo(id);
-    }
-
     @DeleteMapping("/{id}")
-    public boolean deleteTodo(@PathVariable Long id){
-        return todoService.deleteTodo(id);
+    public ResponseEntity<Object> deleteTodo(@PathVariable Long id){
+        return todoService.deleteTodo(id)
+        .map(todo -> ResponseEntity.accepted().body((Object) todo))
+        .orElseGet(() -> ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse(
+                    "Bad Request",
+                    "Invalid or non-existent id",
+                    LocalDateTime.now()
+            ))
+        );
     }
 }
--- todo-list-api/src/main/java/com/todoapirest/todo_list_api/Service/TodoService.java
+++ todo-list-api/src/main/java/com/todoapirest/todo_list_api/Service/TodoService.java
@@ -1,11 +1,13 @@
 package com.todoapirest.todo_list_api.Service;
 import com.todoapirest.todo_list_api.Entity.Todo;
+import com.todoapirest.todo_list_api.Exceptions.Records.ErrorResponse;
 import com.todoapirest.todo_list_api.Repository.TodoRepository;
 import com.todoapirest.todo_list_api.TodoDataTransferObject.TodoCreateRequest;
 import com.todoapirest.todo_list_api.TodoDataTransferObject.TodoUpdateRequest;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,42 +18,60 @@ public class TodoService {
     public TodoService(TodoRepository todoRepository) {this.todoRepository = todoRepository;}
 
 
-    public Todo createTodo(TodoCreateRequest todoRequest){
-        Todo todo = new Todo();
-        todo.setTitle(todoRequest.title());
-        todo.setCompleted(todoRequest.completed());
-        todo.setDescription(todoRequest.description());
-        todo.setPriority(todoRequest.priority());
-
+    public Optional<Todo>  createTodo(TodoCreateRequest todoRequest){
         try {
-            return todoRepository.save(todo);
+            Todo todo = new Todo();
+            todo.setTitle(todoRequest.title());
+            todo.setCompleted(todoRequest.completed());
+            todo.setDescription(todoRequest.description());
+            todo.setPriority(todoRequest.priority());
+            return Optional.of(todoRepository.save(todo));
         } catch (Exception ex) {
             throw new RuntimeException("Error for toDo creation on createTodo() method.", ex);
         }
     }
 
     public Optional<Todo> updateTodo(TodoUpdateRequest todo){
-        return todoRepository.findById(todo.id())
-        .map(existing -> {
-            existing.setTitle(todo.title());
-            existing.setCompleted(todo.completed());
-            existing.setDescription(todo.description());
-            existing.setPriority(todo.priority());
-            return todoRepository.save(existing);
-        });
+        try{
+            return todoRepository.findById(todo.id())
+                    .map(existing -> {
+                        existing.setTitle(todo.title());
+                        existing.setCompleted(todo.completed());
+                        existing.setDescription(todo.description());
+                        existing.setPriority(todo.priority());
+                        return todoRepository.save(existing);
+                    });
+        } catch (Exception ex) {
+            throw new RuntimeException("Error for toDo update on updateTodo() method.", ex);
+        }
     }
 
     public Optional<Todo> listTodo(Long id){
-        return todoRepository.findById(id);
+        try{
+            return todoRepository.findById(id);
+        } catch (Exception ex) {
+            throw new RuntimeException("Error for recover todo on listTodo() method.", ex);
+        }
     }
 
-    public List<Todo> listAllTodos(){
-        return todoRepository.findAll();
+    public Optional<List<Todo>> listAllTodos(){
+        try{
+            return Optional.of(todoRepository.findAll());
+        } catch (Exception ex) {
+            throw new RuntimeException("Error for toDo listing on listAllTodos() method.", ex);
+        }
     }
 
-    public boolean deleteTodo(Long id){
-        todoRepository.deleteById(id);
-
-        return true;
+    public Optional<Todo> deleteTodo(Long id){
+        try{
+            Optional<Todo> todo = listTodo(id);
+            if(todo.isPresent()){
+                todoRepository.deleteById(id);
+                return todo;
+            }
+            return Optional.empty();
+        } catch (Exception ex) {
+            throw new RuntimeException("Error for toDo deletion on deleteTodo() method.", ex);
+        }
     }
 }
