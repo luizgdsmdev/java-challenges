b55a8a61ae97c7b1d4414cac3251494e8e280554
Luiz Messias <luizgdsmdev@gmail.com>
2025-11-14 17:02:49 -0300

    Docs (itau_API_REST_challenge): adding basic structure for itau API challenge.

---
+++ Spring_Boot_API_REST_challenge/src/main/java/com/itau/bank/backend/itau_API_REST_challenge/itau_API_REST_challenge.java
@@ -0,0 +1,13 @@
+package com.itau.bank.backend.itau_API_REST_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class itau_API_REST_challenge {
+
+	public static void main(String[] args) {
+		SpringApplication.run(itau_API_REST_challenge.class, args);
+	}
+
+}
---
+++ Spring_Boot_API_REST_challenge/src/test/java/com/itau/bank/backend/itau_API_REST_challenge/itauAPIRESTchallengeTests.java
@@ -0,0 +1,13 @@
+package com.itau.bank.backend.itau_API_REST_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class itauAPIRESTchallengeTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
